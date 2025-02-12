2b72c346d02a591be2a197e88bc618249658a906
Aman <aman@coreshield.io>
2025-02-13 01:49:09 +0530

    added bento grid

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,10 @@
 import Hero from '../components/Hero';
-
+import { BentoDemo } from '../components/magicui/BentoDemo';
 const Home = () => {
   return(
     <div>
       <Hero />
+      <BentoDemo />
     </div>
   ) ;
 };
